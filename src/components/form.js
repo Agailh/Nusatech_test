51d50af30eeef818b5617c4b795f420f15004ea6
Agailh <agailham86@gmail.com>
2023-01-04 11:48:09 +0700

    commit

---
+++ src/components/form.js
@@ -0,0 +1,33 @@
+const login = [
+  {
+    labelText: "Email",
+    labelFor: "email-address",
+    id: "id-email",
+    name: "email",
+    type: "email",
+    autoComplete: "email",
+    isRequired: true,
+    placeholder: "Email Addres",
+  },
+  {
+    labelText: "password",
+    labelFor: "password",
+    id: "password",
+    name: "password",
+    type: "password",
+    autoComplete: "current-password",
+    isRequired: true,
+    placeholder: "Password",
+  },
+
+  {
+    labelText: "confirm-password",
+    labelFor: "confirm-password",
+    id: "confirm-password",
+    name: "confirm-password",
+    type: "confirm-password",
+    autoComplete: "confirm-current-password",
+    isRequired: true,
+    placeholder: "confirm-Password",
+  },
+];
